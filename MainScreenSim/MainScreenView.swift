c6ac8a29edf003ca7794f42a9d1283481787ac7a
beadevo <bealingual@gmail.com>
2026-05-26 14:03:26 +0800

    tes file

---
+++ MainScreenSim/MainScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  MainScreenView.swift
+//  ada_ch3
+//
+//  Created by Beatrice Deviana on 26/05/26.
+//
+
+import SwiftUI
+
+struct MainScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+} 
+
+#Preview {
+    MainScreenView()
+}
