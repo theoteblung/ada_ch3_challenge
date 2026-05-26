b315a9c1952b5c6b194fb3ad4318e57de19779aa
Keiko <serahkeiko@gmail.com>
2026-05-26 15:37:59 +0800

    info screen

--- ada_ch3/MainView/Keiko/InfoScreen.swift
+++ ada_ch3/MainView/Keiko/InfoScreen.swift
@@ -11,8 +11,34 @@ import SwiftData
 
 struct InfoScreen: View{
     var body: some View{
-        
-            Text("Noise Information")
+        ZStack{
+            Color.black
+                .opacity(0.9)
+                .ignoresSafeArea()
+            
+            Rectangle()
+                .fill(
+                    LinearGradient(colors: [.white, .clear], startPoint: .topLeading, endPoint: .bottomLeading)
+                )
+                .opacity(0.2)
+                .ignoresSafeArea()
+                .frame(width: 500, height: 500)
+                .padding(.bottom, 300)
+                        
+            VStack{
+                HStack{
+                    
+                    
+                    Text("Noise Information")
+                        .foregroundStyle(.white)
+                        .font(.system(size: 20))
+                        .fontWeight(.bold)
+                        .padding(50)
+                }
+                
+                Spacer()
+            }
+        }
     }
     
     
