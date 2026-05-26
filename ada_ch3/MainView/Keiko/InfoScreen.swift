99eb408161ed202978a4e9754612c15bbdc1d8a4
Keiko <serahkeiko@gmail.com>
2026-05-26 15:36:20 +0800

    info screen

---
+++ ada_ch3/MainView/Keiko/InfoScreen.swift
@@ -0,0 +1,23 @@
+//
+//  infoScreen.swift
+//  ada_ch3
+//
+//  Created by Keiko Serah on 26/05/26.
+//
+
+import SwiftUI
+import SwiftData
+
+
+struct InfoScreen: View{
+    var body: some View{
+        
+            Text("Noise Information")
+    }
+    
+    
+}
+
+#Preview{
+    InfoScreen()
+}
