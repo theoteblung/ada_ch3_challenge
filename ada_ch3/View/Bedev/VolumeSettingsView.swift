fc57ae51cc2cf27e2121ad75cc7524ba5140d1b8
beadevo <bealingual@gmail.com>
2026-06-02 16:05:26 +0800

    vol sliders

---
+++ ada_ch3/View/Bedev/VolumeSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  VolumeSettingsView.swift
+//  ada_ch3
+//
+//  Created by Beatrice Deviana on 02/06/26.
+//
+
+import SwiftUI
+
+struct VolumeSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VolumeSettingsView()
+}
